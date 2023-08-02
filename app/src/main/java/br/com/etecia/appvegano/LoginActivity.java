5207c38e518571fe4aa1b7288fe0f2df22c1c241
kayoGc <kayivicti@gmail.com>
2023-08-01 21:31:24 -0300

    opa

--- app/src/main/java/br/com/etecia/appvegano/LoginActivity.java
+++ app/src/main/java/br/com/etecia/appvegano/LoginActivity.java
@@ -3,12 +3,31 @@ package br.com.etecia.appvegano;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnEntrar, btnSair, btnCad;
+    TextInputEditText txtEmail, txtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnSair = findViewById(R.id.btnSair);
+        btnCad = findViewById(R.id.btnCad);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha);
+
+    btnSair.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //finish();
+        }
+    });
     }
 }
---
+++ app/src/main/java/br/com/etecia/appvegano/SobreNosActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appvegano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SobreNosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sobre_nos_layout);
+    }
+}
