fb86767e53385dc59bf574d19351420f799b5af9
kayoGc <kayivicti@gmail.com>
2023-08-08 21:36:19 -0300

    lista filmes - fazendo viewHolder

---
+++ app/src/main/java/br/com/etecia/appvegano/MyAdapter.java
@@ -0,0 +1,54 @@
+package br.com.etecia.appvegano;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+ // extemde primerio com o RecyclerView.Apadter
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    // da alt + enter se tiver vermelho
+    private Context mContexto;
+
+    private List<Filmes> lstFilmes;
+
+     @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView idTitutloFilmes;
+        ImageView idIagemFilmes;
+        CardView idCardFilmes;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            idTitutloFilmes = itemView.findViewById(R.id.idTituloFilmes);
+            idIagemFilmes = itemView.findViewById(R.id.idImgFilmes);
+            idCardFilmes = itemView.findViewById(R.id.cardFilmes);
+
+        }
+    }
+
+}
